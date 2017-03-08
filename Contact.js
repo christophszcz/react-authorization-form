e93a908b81843cd3b5d0285e154f89f109e81c79
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-07 22:59:37 -0500

    Added the contact component.

---
+++ Contact.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Contact = React.createClass(
+	);
+
+ReactDOM.render(
+	< Contact />,
+	document.getElementById('app')
+	);
